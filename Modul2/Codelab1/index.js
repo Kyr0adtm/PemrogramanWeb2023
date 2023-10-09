f71e2f1ad4789ad27cee7b0f0148cc4e3ed310bf
Yudanto Aditama <89512935+Kyr0adtm@users.noreply.github.com>
2023-10-09 16:02:45 +0700

    commit

---
+++ Modul2/Codelab1/index.js
@@ -0,0 +1,11 @@
+function jumlahkan() {
+  var bilangan1 = document.getElementById("bilangan1").value;
+  var bilangan2 = document.getElementById("bilangan2").value;
+  var hasil = parseInt(bilangan1) + parseInt(bilangan2);
+  alert("Hasil Penjualan " + " = " + hasil);
+}
+
+function ulang() {
+  document.getElementById("bilangan1").value = "";
+  document.getElementById("bilangan2").value = "";
+}
